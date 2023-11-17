f785901ead895fc55b0bd03dd3916a431bcb5b74
rphong <richard.phong424@gmail.com>
2023-11-16 18:13:59 -0600

    feat: add tank to scene

--- src/tengin.cpp
+++ src/tengin.cpp
@@ -9,6 +9,7 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
+#include <array>
 #include "utility/shader.hpp"
 
 const unsigned int WIDTH = 1200; 
@@ -40,26 +41,50 @@ int main() {
 
     Shader shader("./shaders/vertex.glsl", "./shaders/fragment.glsl");
 
-    const float vertices[] = {
+    const float bgVert[] = {
         -0.8f, -0.8f, 0.0f, 0.0f, 0.0f,
         -0.8f,  0.8f, 0.0f, 0.0f, 1.0f, 
          0.8f,  0.8f, 0.0f, 1.0f, 1.0f,
          0.8f, -0.8f, 0.0f, 1.0f, 0.0f
+    };
+    const float tankVert[] = {
+        -0.2f, -0.2f, 0.0f, 0.0f, 0.0f,
+        -0.2f, -0.1f, 0.0f, 0.0f, 1.0f, 
+        -0.1f, -0.1f, 0.0f, 1.0f, 1.0f,
+        -0.1f, -0.2f, 0.0f, 1.0f, 0.0f
     };
     unsigned int indicies[] = {
         0, 1, 2,
         2, 0, 3
     };
 
-    unsigned int VAO, VBO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    unsigned int EBO;
+    // To draw multiple objects, bind corresponding VAO, then draw
+    std::array<GLuint, 2> VAOs;
+    std::array<GLuint, 2> VBOs;
+    glGenVertexArrays(VAOs.size(), &VAOs[0]);
+    glGenBuffers(VBOs.size(), &VBOs[0]);
     glGenBuffers(1, &EBO);
 
-    glBindVertexArray(VAO);
+    glBindVertexArray(VAOs[0]);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(bgVert), bgVert, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+    glBindVertexArray(VAOs[1]);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(tankVert), tankVert, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
@@ -70,9 +95,10 @@ int main() {
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    unsigned int texture1;
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    std::array<GLuint, 2> textures;
+    glGenTextures(textures.size(), &textures[0]);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
     // Wrapping parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -92,28 +118,59 @@ int main() {
     }
     stbi_image_free(data);
 
-    shader.use();
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+    // Wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // Filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    data = stbi_load("./textures/green_tank.png", &width, &height, &nrChannels, STBI_rgb_alpha);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
+                     GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
     shader.setInt("texture1", 0);
+
+    shader.use();
 
     while (!glfwWindowShouldClose(window)) {
         // Rendering commands
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // Set textures
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-
         shader.use();
-        glBindVertexArray(VAO);
+        
+        // Enable transparency
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        // Draw floor
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textures[0]);
+
+        glBindVertexArray(VAOs[0]);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // Draw tanks
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textures[1]);
+        shader.setInt("texture1", 0);
+
+        glBindVertexArray(VAOs[1]);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // Check and call events & swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(VAOs.size(), &VAOs[0]);
+    glDeleteBuffers(VBOs.size(), &VBOs[0]);
     glDeleteBuffers(1, &EBO);
     glfwTerminate();
 
