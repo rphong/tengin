f94d8d5e54317d6ab2465b92cca91123363cdf23
rphong <richard.phong424@gmail.com>
2024-04-03 15:42:36 -0500

    refactor: abstract collision implementation

--- tengin/src/entities/entity.cpp
+++ tengin/src/entities/entity.cpp
@@ -1,62 +1,79 @@
 #include "entity.hpp"
 
+struct Entity::Projection {
+  Projection(std::ranges::min_max_result<float> minmax) {
+    const auto [low, high] = minmax;
+    min = low;
+    max = high;
+  }
+  [[nodiscard]] constexpr float overlap(const Projection& o) {
+    const float lower = std::max(min, o.min);
+    const float upper = std::min(max, o.max);
+
+    return lower <= upper ? upper - lower : 0;
+  }
+
+  float min;
+  float max;
+};
+
 glm::vec2 Entity::colldies(const Entity& other) {
-    const std::vector<glm::vec2> o_hitbox = other.getHitbox();
+  const std::vector<glm::vec2> o_hitbox = other.getHitbox();
 
-    const auto m_edges = m_hitbox 
-        | std::views::slide(2) 
-        | std::views::transform(
-            [](const auto& pair) {
-                return glm::normalize(pair[1] - pair[0]);
-            });
+  const auto m_edges = m_hitbox | std::views::slide(2) |
+                       std::views::transform([](const auto& pair) {
+                         return glm::normalize(pair[1] - pair[0]);
+                       });
 
-    const auto o_edges = o_hitbox 
-        | std::views::slide(2) 
-        | std::views::transform(
-            [](const auto& pair) {
-                return glm::normalize(pair[1] - pair[0]); 
-            });
+  const auto o_edges = o_hitbox | std::views::slide(2) |
+                       std::views::transform([](const auto& pair) {
+                         return glm::normalize(pair[1] - pair[0]);
+                       });
 
-    const auto m_axis = m_edges
-        | std::views::transform(
-            [](const auto& edge) {
-                return glm::vec2{-edge.y, edge.x};
-            });
+  const auto m_axis = m_edges | std::views::transform([](const auto& edge) {
+                        return glm::vec2{-edge.y, edge.x};
+                      });
 
-    const auto o_axis = o_edges
-        | std::views::transform(
-            [](const auto& edge) {
-                return glm::vec2{-edge.y, edge.x};
-            });
+  const auto o_axis = o_edges | std::views::transform([](const auto& edge) {
+                        return glm::vec2{-edge.y, edge.x};
+                      });
 
-    glm::vec2 mtvAxis;
-    float mtvOverlap = INT_MAX;
+  glm::vec2 mtvAxis;
+  float mtvOverlap = INT_MAX;
 
-    for(const auto& axis: m_axis) {
-        Projection p1 = project(m_hitbox, axis);
-        Projection p2 = project(o_hitbox, axis);
+  for (const auto& axis : m_axis) {
+    Projection p1 = project(m_hitbox, axis);
+    Projection p2 = project(o_hitbox, axis);
 
-        const float overlap = p1.overlap(p2);
-        if(overlap == 0) return {0, 0};
-        else if(overlap < mtvOverlap) {
-            mtvAxis = axis;
-            mtvOverlap = overlap;
-        }
+    const float overlap = p1.overlap(p2);
+    if (overlap == 0)
+      return {0, 0};
+    else if (overlap < mtvOverlap) {
+      mtvAxis = axis;
+      mtvOverlap = overlap;
     }
+  }
 
-    for(const auto& axis: o_axis) {
-        Projection p1 = project(m_hitbox, axis);
-        Projection p2 = project(o_hitbox, axis);
+  for (const auto& axis : o_axis) {
+    Projection p1 = project(m_hitbox, axis);
+    Projection p2 = project(o_hitbox, axis);
 
-        const float overlap = p1.overlap(p2);
-        if(overlap == 0) return {0, 0};
-        else if(overlap < mtvOverlap) {
-            mtvAxis = axis;
-            mtvOverlap = overlap;
-        }
+    const float overlap = p1.overlap(p2);
+    if (overlap == 0)
+      return {0, 0};
+    else if (overlap < mtvOverlap) {
+      mtvAxis = axis;
+      mtvOverlap = overlap;
     }
+  }
 
-    return mtvAxis * mtvOverlap;
+  return mtvAxis * mtvOverlap;
 }
 
-
+Entity::Projection Entity::project(const std::vector<glm::vec2>& hitbox,
+                                   const glm::vec2& axis) const {
+  const auto dotProducts =
+      hitbox | std::views::transform(
+                   [&axis](const auto& pt) { return glm::dot(pt, axis); });
+  return Projection(std::ranges::minmax(dotProducts));
+}
--- tengin/src/entities/entity.hpp
+++ tengin/src/entities/entity.hpp
@@ -9,54 +9,31 @@
 #include <algorithm>
 
 class Entity {
-   public:
-    constexpr Entity(const glm::vec2& pos,
-                     const unsigned int& collisionLevel)
-        : m_pos(pos), m_collisionLevel(collisionLevel){};
-    void constexpr nudge(glm::vec2 translation) {
-        m_pos += translation;
-        updateHitbox();
-    }
-    glm::vec2 colldies(const Entity& other);
+ public:
+  constexpr Entity(const glm::vec2& pos, const unsigned int& collisionLevel)
+      : m_pos(pos), m_collisionLevel(collisionLevel){};
+  void constexpr nudge(glm::vec2 translation) {
+    m_pos += translation;
+    updateHitbox();
+  }
+  glm::vec2 colldies(const Entity& other);
 
-   protected:
-    void constexpr virtual updateHitbox() = 0;
-    [[nodiscard]] constexpr std::vector<glm::vec2> getHitbox() const {
-        return m_hitbox;
-    }
-    [[nodiscard]] constexpr unsigned int getCollisionLevel() const {
-        return m_collisionLevel;
-    }
+ protected:
+  void constexpr virtual updateHitbox() = 0;
+  [[nodiscard]] constexpr std::vector<glm::vec2> getHitbox() const {
+    return m_hitbox;
+  }
+  [[nodiscard]] constexpr unsigned int getCollisionLevel() const {
+    return m_collisionLevel;
+  }
 
-    glm::vec2 m_pos;
-    std::vector<glm::vec2> m_hitbox;
+  glm::vec2 m_pos;
+  std::vector<glm::vec2> m_hitbox;
 
-   private:
-    struct Projection {
-        Projection(std::ranges::min_max_result<float> minmax) {
-            const auto [low, high] = minmax;
-            min = low;
-            max = high;
-        }
-        [[nodiscard]] constexpr float overlap(const Projection& o) {
-            const float lower = std::max(min, o.min);
-            const float upper = std::min(max, o.max);
+ private:
+  struct Projection;
 
-            return lower <= upper ? upper - lower : 0;
-        }
-
-        float min;
-        float max;
-    };
-
-    [[nodiscard]] Projection project(const std::vector<glm::vec2>& hitbox,
-                                     const glm::vec2& axis) const {
-        const auto dotProducts =
-            hitbox | std::views::transform([&axis](const auto& pt) {
-                return glm::dot(pt, axis);
-            });
-        return Projection(std::ranges::minmax(dotProducts));
-    }
-
-    const unsigned int m_collisionLevel;
+  [[nodiscard]] Projection project(const std::vector<glm::vec2>& hitbox,
+                                   const glm::vec2& axis) const;
+  const unsigned int m_collisionLevel;
 };
